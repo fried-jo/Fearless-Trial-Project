57d4bf4ae0ac93bbe5fd00b8c3233d2c1cdc546b
fried-jo <101134988+fried-jo@users.noreply.github.com>
2022-10-08 14:25:10 -0400

    Added create item controller

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -2,9 +2,20 @@ const mockDatabase = [];
 
 const itemController = {};
 
-//MVP
-itemController.createItem = (req, res, next) => {
+let globalIdCounter = 1;
 
+//MVP
+
+itemController.createItem = (req, res, next) => {
+    // console.log(req.query)
+    const { newName }  = req.query;
+    res.locals.newItem = {
+        //assigns the ID, then post-increments the global ID counter
+        id: globalIdCounter++,
+        name: newName
+    };
+    mockDatabase.push(res.locals.newItem);
+    console.log('Database updated: ', mockDatabase);
     return next();
 }
 
