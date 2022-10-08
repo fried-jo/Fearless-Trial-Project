49c651937cb92fe783a15d064a621858dd8005a0
fried-jo <101134988+fried-jo@users.noreply.github.com>
2022-10-08 15:09:45 -0400

    Added env file and dependency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const itemRouter = require('./routes/items.js');
 
 //Parse JSON fromm incoming request body
