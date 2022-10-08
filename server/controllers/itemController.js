25685fd6aab882ddbe1128d476df46734ec857c4
fried-jo <101134988+fried-jo@users.noreply.github.com>
2022-10-08 14:22:59 -0400

    Added skeleton structure for item controller

---
+++ server/controllers/itemController.js
@@ -0,0 +1,36 @@
+const mockDatabase = [];
+
+const itemController = {};
+
+//MVP
+itemController.createItem = (req, res, next) => {
+
+    return next();
+}
+
+itemController.getItems = (req, res, next) => {
+    return next();
+}
+
+itemController.deleteItems = (req, res, next) => {
+    return next();
+}
+
+
+//Stretch
+itemController.getItemByID = (req, res, next) => {
+
+    return next();
+}
+
+itemController.updateItem = (req, res, next) => {
+
+    return next()
+}
+
+itemController.deleteItem = (req, res, next) => {
+
+    return next();
+}
+
+module.exports = itemController;
